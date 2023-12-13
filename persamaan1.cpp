07dffe1d97cccdb93c03a70b4c2e22cd50b52bb9
xiewaavonsy <150318527+xiewaavonsy@users.noreply.github.com>
2023-12-13 16:31:43 +0700

    Add files via upload

---
+++ persamaan1.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+    
+float fpersamaan1(int a,int b,int c){
+    float hasil;
+    hasil = (4*a*b)/(7*c);
+    return hasil;
+}
+int main (){
+    int a,b,c;
+    float hasil;
+    cout<<"Masukan Nilai A :";
+    cin>>a;
+    cout<<"Masukan Nilai B :";
+    cin>>b;
+    cout<<"Masukan Nilai C :";
+    cin>>c;
+    hasil = fpersamaan1(a,b,c);
+    cout<<"Hasil Persamaan 1 = "<<hasil<<endl<<endl;  
+    system("pause");
+    return 0;  
+}
